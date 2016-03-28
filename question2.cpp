5144b2c83514c83b4f34fd94bcc6ee68b4903b8c
Ruvania <214521716@stu.ukzn.ac.za>
2016-03-28 12:20:18 +0200

    Time

--- question2.cpp
+++ question2.cpp
@@ -1,12 +1,52 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name:Ruvania Naicker
+// Student No: 214521716
+// Date: 28/03/2016
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
-
 using namespace std;
+
+class Time
+{
+private:
+
+public: Time();
+		int hours;
+		int mins;
+		int hrs;
+		int min;
+		void set(int, int);
+		void get(int, int);
+};
+
+Time::Time(int hours, int mins)
+{
+
+};
+
+void Time::set(int hours,int mins)
+{
+	hrs = 0;
+	min = 0;
+
+	if (hours<23 && hours>=0)
+	{
+		hrs = hours; 
+	}
+	else cout << "Invalid hours entered!" << endl; 
+
+	if (mins < 60 && mins >= 0)
+	{
+		min = mins;
+	}
+	else cout << "Invalid minutes entered!" << endl;
+}
+
+void Time::get(int hours, int mins)
+{
+
+}
 
 // ***** QUESTION 2 *****
 // Question 2 consists of 3 parts
